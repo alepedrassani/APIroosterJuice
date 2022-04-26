88326dac57d742bb045988e0d46c1aa183d21e78
Alexandre Couss Pedrassani <alexandrepedrassani1@gmail.com>
2022-04-25 21:37:13 -0300

    incluido ProcFile

--- ecommerce-local.session.sql
+++ ecommerce-local.session.sql
@@ -1,7 +1,1 @@
-show TABLEs;
-
-create table usuarios (
-    id_usuario INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100),
-    senha VARCHAR(100)
-)
+select * from Produtos
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const UsuariosController = require('../controllers/usuarios-controller')
 
 router.post('/cadastro', UsuariosController.CadastrarUmUsuario)
